1359893bdf0bafd36ef91a70d1936d68a089d949
wojteq18 <279730@student.pwr.edu.pl>
2025-04-12 12:05:10 +0200

    files and text are alright

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,59 @@
-use reqwest::{Client, StatusCode};
-use serde::Deserialize;
-use std::time::Duration;
+mod translate_text;
+
+use whatlang::detect;
 use std::{fs, io};
-
-#[derive(Deserialize, Debug)]
-pub struct HttpResponse {
-    responseData: Option<TranslateResponse>,
-    responseStatus: Option<u16>
-}
-
-#[derive(Deserialize, Debug)]
-struct TranslateResponse {
-    translatedText: String,
-}
-
-async fn translate_text(text: &str, api_key: &str, pair: &str) -> Result<String, String> {
-    let client = Client::new();
-
-    let response = client.get("https://api.mymemory.translated.net/get")
-    .query(&[("q", text), ("langpair", pair), ("key", api_key)])
-        .timeout(Duration::new(30, 0))
-        .send()
-        .await;
-
-    match response {
-        Ok(resp) => {
-            if resp.status() == StatusCode::OK {
-                let full_response: HttpResponse = resp.json().await.unwrap();
-                if let Some(translated_response) = full_response.responseData {
-                    Ok(translated_response.translatedText)
-                } else {
-                    Err("No translation found".to_string())
-                }
-            } else {
-                Err(format!("Error: {}", resp.status()))
-
-            }
-        }
-        Err(_) => {
-            Err("Failed to send request".to_string())
-        }
-    }
-}    
-
-fn get_pair() -> String {
-    let mut pair = String::new();
-    println!("Enter the language pair (e.g., 'en|es' for English to Spanish): ");
-    io::stdin().read_line(&mut pair).expect("Failed to read line");
-    pair.trim().to_string()
-}
+use translate_text::{translate_text};
+    
 
 fn get_text() -> String {
     let mut text = String::new();
-    println!("Enter the text to translate: ");
     io::stdin().read_line(&mut text).expect("Failed to read line");
     text.trim().to_string()
 }
 
-fn get_file_path() -> String {
-    let mut text = String::new();
-    println!("Enter the file path to translate: ");
-    io::stdin().read_line(&mut text).expect("Failed to read line");
-    text.trim().to_string()
-}
 
 #[tokio::main]
 async fn main() { 
-    let pair = get_pair();
-    let path = get_file_path();
     let api_key = "YOUR_API_KEY"; // Replace with your actual API key
-    //let text = get_text();
+    println!("Type action to perform: 1 - translate text; 2 - translate text file");
+    let mut action = String::new();
+    io::stdin().read_line(&mut action).expect("Failed to read line");
 
-    let text = std::fs::read_to_string(&path).expect("Unable to read file");
-    fs::write(&path, "").unwrap();
-    match translate_text(&text, api_key, &pair).await {
-        Ok(translated_text) => {
-            fs::write(&path, translated_text).expect("Unable to write file");
-            println!("Wszystko top");
+    let action_num: i32 = action.trim().parse().expect("Please type a number!");
+    
+
+    match action_num {
+        1 => {
+            println!("Type pair of language - first language to translate from, second language to translate to: ex (pl|en)");
+            let pair = get_text();
+            println!("Type text to translate:");
+            let text = get_text();
+
+            match translate_text(&text, &api_key, &pair).await {
+                Ok(translated_text) => {
+                    println!("Translated text: {}", translated_text);
+                }
+                Err(e) => eprintln!("Error: {}", e),
+            }
         }
-        Err(e) => eprintln!("Error: {}", e),
+        2 => {
+            println!("Type pair of language - first language to translate from, second language to translate to: ex (pl|en)");
+            let pair = get_text();
+            println!("Type path to file:");
+            let path = get_text();
+            let text = std::fs::read_to_string(&path).expect("Unable to read file");
+
+            match translate_text(&text, api_key, &pair).await {
+                Ok(translated_text) => {
+                    fs::write(&path, translated_text).expect("Unable to write file");
+                    println!("File translated");
+                }
+                Err(e) => eprintln!("Error: {}", e),
+            }
+        }
+        _ => {
+            println!("Invalid action");
+            return;
+        }
     }
 }
---
+++ src/translate_text.rs
@@ -0,0 +1,44 @@
+use reqwest::{Client, StatusCode};
+use serde::Deserialize;
+use std::time::Duration;
+use whatlang::detect;
+
+#[derive(Deserialize, Debug)]
+pub struct HttpResponse {
+    responseData: Option<TranslateResponse>,
+    responseStatus: Option<u16>
+}
+
+#[derive(Deserialize, Debug)]
+struct TranslateResponse {
+    translatedText: String,
+}
+
+pub async fn translate_text(text: &str, api_key: &str, pair: &str) -> Result<String, String> {
+    let client = Client::new();
+
+    let response = client.get("https://api.mymemory.translated.net/get")
+    .query(&[("q", text), ("langpair", pair), ("key", api_key)])
+        .timeout(Duration::new(30, 0))
+        .send()
+        .await;
+
+    match response {
+        Ok(resp) => {
+            if resp.status() == StatusCode::OK {
+                let full_response: HttpResponse = resp.json().await.unwrap();
+                if let Some(translated_response) = full_response.responseData {
+                    Ok(translated_response.translatedText)
+                } else {
+                    Err("No translation found".to_string())
+                }
+            } else {
+                Err(format!("Error: {}", resp.status()))
+
+            }
+        }
+        Err(_) => {
+            Err("Failed to send request".to_string())
+        }
+    }
+}
